6caf393d7c0e5b108ab73ce1971a8909f0ee9571
Georg Wirtenberger <georg.wirtenberger@icloud.com>
2026-08-16 18:57:18 +0200

    test: walk one question from upload to a cited answer

--- src/cora/domain/query_plan.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-from cora.domain.metadata_filter import MetadataFilter
-
-
-@dataclass(frozen=True)
-class QueryPlan:
-    queries: tuple[str, ...]
-    metadata_filter: MetadataFilter | None = None
